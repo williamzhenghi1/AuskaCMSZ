3d4a0dee43853abe61b4b76cd26b5949bad5ea3f
Hakase Toshi <zhenghongye2003@163.com>
2019-01-29 01:28:06 +0800

    设置了jpa内容

--- pic/src/main/java/com/asuka/pic/PicApplication.java
+++ pic/src/main/java/com/asuka/pic/PicApplication.java
@@ -2,10 +2,16 @@ package com.asuka.pic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableEurekaClient
+@ComponentScan(basePackages = {"com.asuka.*"})
+@EnableJpaRepositories("com.asuka.Repositry")
+@EntityScan("com.asuka.pojo")
 public class PicApplication {
 
     public static void main(String[] args) {
